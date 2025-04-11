45f90c38eab2f8989c7481b695d9b611b36aa353
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-10 22:52:39 -0300

    move scraping auth to utils

--- data/scrape_all_awarded.py
+++ data/scrape_all_awarded.py
@@ -1,60 +1,7 @@
 from typing import Dict
-from playwright.sync_api import sync_playwright
 import json
 import httpx
-from fake_useragent import UserAgent
-from utils import resolve_proxy_ip, get_proxy_conf
-
-
-def launch_browser_and_get_auth(proxy_conf: Dict[str, str]):
-    TARGET_URL = "https://procurement-portal.novascotia.ca/tenders"
-    WATCH_REQUEST = (
-        "https://procurement-portal.novascotia.ca/procurementui/authenticate"
-    )
-    jwt_token = None
-    ua = UserAgent(platforms="desktop").random
-    proxy_conf = {
-        "server": f"http://{proxy_conf['server']}",
-        "username": proxy_conf["username"],
-        "password": proxy_conf["password"],
-    }
-
-    with sync_playwright() as p:
-        browser = p.chromium.connect_over_cdp("http://localhost:9222")
-        context = browser.new_context(
-            proxy=proxy_conf,
-            user_agent=ua,
-            viewport={"width": 1280, "height": 800},
-        )
-        context.add_init_script("""
-        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
-        Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-        Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
-        """)
-
-        def on_response(response):
-            nonlocal jwt_token
-            if response.url == WATCH_REQUEST:
-                body = response.text()
-                data = json.loads(body)
-                jwt_token = data.get("jwttoken")
-
-        context.on("response", on_response)
-
-        page = context.new_page()
-        page.goto(TARGET_URL, timeout=60000, wait_until="domcontentloaded")
-        page.wait_for_timeout(2000)
-        cookies = context.cookies()
-        browser.close()
-
-    if not jwt_token:
-        raise Exception("No token received")
-
-    return {
-        "jwt": jwt_token,
-        "cookies": cookies,
-        "user_agent": ua,
-    }
+from utils import resolve_proxy_ip, get_proxy_conf, launch_browser_and_get_auth
 
 
 def send_authenticated_request(auth_data: Dict[str, str]):
