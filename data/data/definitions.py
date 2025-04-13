e7f5d826221469d9955dc13b0a8279be544f8d36
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-13 11:12:01 -0300

    start compose setup

--- data/data/definitions.py
+++
@@ -1,6 +0,0 @@
-from dagster import Definitions
-
-from ingestion.definitions import defs as ingestion_definitions
-
-
-defs = Definitions.merge(ingestion_definitions)
--- data/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="data",
-    packages=find_packages(),
-    install_requires=["dagster", "dagster-cloud"],
-    extras_require={"dev": ["dagster-webserver"]},
-)
