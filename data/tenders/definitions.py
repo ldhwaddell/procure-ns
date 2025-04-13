add8bc08b3a71266af047dd9b79f20b18b3a99b6
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-13 18:48:30 -0300

    start sqlalchemy testing

--- data/tenders/definitions.py
+++ data/tenders/definitions.py
@@ -1,21 +1,24 @@
 import dagster as dg
 from tenders.resources import DataWarehouseResource
-from dagster_docker import docker_executor
+from sqlalchemy.sql import text
+# from dagster_docker import docker_executor
 
 
 @dg.asset(compute_kind="dwh", group_name="ingestion")
 def test_dwh(dwh: DataWarehouseResource) -> dg.MaterializeResult:
-    with dwh.connect() as conn:
-        with conn.cursor() as cursor:
-            # Built-in query to list tables in the current schema (should return empty if no user tables exist)
-            cursor.execute("""
-                SELECT table_name
-                FROM information_schema.tables
-                WHERE table_schema = 'public';
-            """)
+    Session = dwh.get_session()
+    with Session() as session:
+        result = session.execute(
+            text("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public';
+        """)
+        )
 
-            tables = cursor.fetchall()
-            print("Tables in public schema:", tables)
+        tables = result.fetchall()
+        table_names = [row[0] for row in tables]
+        print("Tables in public schema:", table_names)
 
     return dg.MaterializeResult(
         metadata={
@@ -24,17 +27,36 @@ def test_dwh(dwh: DataWarehouseResource) -> dg.MaterializeResult:
         }
     )
 
+    # with dwh.connect() as conn:
+    #     with conn.cursor() as cursor:
+    #         # Built-in query to list tables in the current schema (should return empty if no user tables exist)
+    #         cursor.execute("""
+    #             SELECT table_name
+    #             FROM information_schema.tables
+    #             WHERE table_schema = 'public';
+    #         """)
+    #
+    #         tables = cursor.fetchall()
+    #         print("Tables in public schema:", tables)
+    #
+    # return dg.MaterializeResult(
+    #     metadata={
+    #         "table_count": dg.MetadataValue.int(len(tables)),
+    #         "test": dg.MetadataValue.md("md test"),
+    #     }
+    # )
 
-dwh_job = dg.define_asset_job(
-    name="dwh_job", selection=["test_dwh"], executor_def=docker_executor
-)
 
-dwh_job_2 = dg.define_asset_job(name="dwh_job_2", selection=["test_dwh"])
-
+# dwh_job = dg.define_asset_job(
+#     name="dwh_job", selection=["test_dwh"], executor_def=docker_executor
+# )
+#
+# dwh_job_2 = dg.define_asset_job(name="dwh_job_2", selection=["test_dwh"])
+#
 
 defs = dg.Definitions(
     assets=[test_dwh],
-    jobs=[dwh_job, dwh_job_2],
+    # jobs=[dwh_job, dwh_job_2],
     resources={
         "dwh": DataWarehouseResource(
             username=dg.EnvVar("DWH_POSTGRES_USER"),
--- data/tenders/resources.py
+++ data/tenders/resources.py
@@ -1,6 +1,23 @@
 import dagster as dg
 
-import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+
+# class DataWarehouseResource(dg.ConfigurableResource):
+#     username: str
+#     password: str
+#     db: str
+#
+#     def connect(self) -> psycopg2.extensions.connection:
+#         return psycopg2.connect(
+#             host="dwh",  # matches `hostname` and `container_name`
+#             port=5432,  # default Postgres port
+#             user=self.username,
+#             password=self.password,
+#             dbname=self.db,
+#         )
+#
 
 
 class DataWarehouseResource(dg.ConfigurableResource):
@@ -8,11 +25,11 @@ class DataWarehouseResource(dg.ConfigurableResource):
     password: str
     db: str
 
-    def connect(self) -> psycopg2.extensions.connection:
-        return psycopg2.connect(
-            host="dwh",  # matches `hostname` and `container_name`
-            port=5432,  # default Postgres port
-            user=self.username,
-            password=self.password,
-            dbname=self.db,
+    def _url(self) -> str:
+        return (
+            f"postgresql+psycopg2://{self.username}:{self.password}@dwh:5432/{self.db}"
         )
+
+    def get_session(self) -> sessionmaker[Session]:
+        engine = create_engine(self._url(), echo=False)
+        return sessionmaker(bind=engine)
