3840f5a671551725a0d6ca5f225e708209e84a00
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-13 19:20:44 -0300

    start proxy resoruce

--- data/tenders/resources.py
+++ data/tenders/resources.py
@@ -1,7 +1,12 @@
+import socket
+import random
+
 import dagster as dg
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+
+from typing import Dict
 
 
 class DataWarehouseResource(dg.ConfigurableResource):
@@ -17,3 +22,21 @@ class DataWarehouseResource(dg.ConfigurableResource):
     def get_session(self) -> sessionmaker[Session]:
         engine = create_engine(self._url(), echo=False)
         return sessionmaker(bind=engine)
+
+
+class ProxyResource(dg.ConfigurableResource):
+    username: str
+    password: str
+
+    def _resolve_proxy_ip(self) -> str:
+        return socket.gethostbyname("brd.superproxy.io")
+
+    def get_proxy_conf(self) -> Dict[str, str]:
+        port = 33335
+        session_id = str(random.random())
+
+        return {
+            "server": f"{self._resolve_proxy_ip()}:{port}",
+            "username": f"{self.username}-session-{session_id}",
+            "password": self.password,
+        }
