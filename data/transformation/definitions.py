60c0a3163ea82048ceeaea42b73685a42801e729
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-15 23:26:26 -0300

    change manifest finding

--- data/transformation/definitions.py
+++ data/transformation/definitions.py
@@ -9,25 +9,28 @@ transformation_project = DbtProject(
     project_dir=Path(__file__).joinpath("..", "dbt_transform").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "dbt_project").resolve(),
 )
-transformation_project.prepare_if_dev()
-
 
 # If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
 # Otherwise, we expect a manifest to be present in the project's target directory.
 
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-    # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
-    dbt_parse_invocation = DbtCliResource(project_dir=transformation_project).cli(
-        ["parse"]
-    )
-    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
-else:
-    dbt_manifest_path = transformation_project.project_dir.joinpath(
-        "target", "manifest.json"
-    )
+# if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+#     # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
+#     dbt_parse_invocation = (
+#         DbtCliResource(project_dir=transformation_project).cli(["parse"]).wait()
+#     )
+#     dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+# else:
+#     dbt_manifest_path = transformation_project.project_dir.joinpath(
+#         "target", "manifest.json"
+#     )
+#
+
+dbt_manifest_path = transformation_project.project_dir.joinpath(
+    "target", "manifest.json"
+)
 
 
-@dbt_assets(manifest=transformation_project.manifest_path)
+@dbt_assets(manifest=dbt_manifest_path)
 def transformation_dbt_assets(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
@@ -42,9 +45,7 @@ def example_asset(context: dg.AssetExecutionContext):
 
 defs = dg.Definitions(
     assets=[example_asset, transformation_dbt_assets],
-    resources=(
-        {
-            "dbt": DbtCliResource(project_dir=transformation_project),
-        },
-    ),
+    resources={
+        "dbt": DbtCliResource(project_dir=transformation_project),
+    },
 )
