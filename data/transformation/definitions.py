120c3ad9526731d1dd01b596d8266c6b6bc16cf7
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-15 22:30:37 -0300

    build manifest

--- data/transformation/definitions.py
+++ data/transformation/definitions.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import dagster as dg
@@ -9,6 +10,21 @@ transformation_project = DbtProject(
     packaged_project_dir=Path(__file__).joinpath("..", "dbt-project").resolve(),
 )
 transformation_project.prepare_if_dev()
+
+
+# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
+# Otherwise, we expect a manifest to be present in the project's target directory.
+
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
+    dbt_parse_invocation = DbtCliResource(project_dir=transformation_project).cli(
+        ["parse"]
+    )
+    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+else:
+    dbt_manifest_path = transformation_project.project_dir.joinpath(
+        "target", "manifest.json"
+    )
 
 
 @dbt_assets(manifest=transformation_project.manifest_path)
