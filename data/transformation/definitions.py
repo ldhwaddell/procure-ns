0df6f6a69a23486aa437fc8721932266518555b1
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-16 20:51:48 -0300

    test prepare-and-package dbt

--- data/transformation/definitions.py
+++ data/transformation/definitions.py
@@ -12,29 +12,30 @@ transformation_project = DbtProject(
     packaged_project_dir=Path(__file__).joinpath("..", "dbt_project").resolve(),
 )
 
-log.info(f"project_dur: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
-log.info(f"packaged_dir: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
+# log.info(f"project_dur: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
+# log.info(f"packaged_dir: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
+#
+# # If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
+# # Otherwise, we expect a manifest to be present in the project's target directory.
+#
+# if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+#     log.info("PARSE ENV VAR SET")
+#     # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
+#     dbt_parse_invocation = (
+#         DbtCliResource(project_dir=transformation_project).cli(["parse"]).wait()
+#     )
+#     log.info(dbt_parse_invocation)
+#     dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+#     log.info(dbt_manifest_path)
+# else:
+#     dbt_manifest_path = transformation_project.project_dir.joinpath(
+#         "target", "manifest.json"
+#     )
+#     log.info(dbt_manifest_path)
+#
 
-# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
-# Otherwise, we expect a manifest to be present in the project's target directory.
 
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-    log.info("PARSE ENV VAR SET")
-    # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
-    dbt_parse_invocation = (
-        DbtCliResource(project_dir=transformation_project).cli(["parse"]).wait()
-    )
-    log.info(dbt_parse_invocation)
-    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
-    log.info(dbt_manifest_path)
-else:
-    dbt_manifest_path = transformation_project.project_dir.joinpath(
-        "target", "manifest.json"
-    )
-    log.info(dbt_manifest_path)
-
-
-@dbt_assets(manifest=dbt_manifest_path)
+@dbt_assets(manifest=Path("target", "manifest.json"))
 def transformation_dbt_assets(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
