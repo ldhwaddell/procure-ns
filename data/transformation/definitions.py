42461a9f2f39ea8aa24578dba6511ef6f32c7eb6
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-15 23:29:34 -0300

    undo, add logging for debug

--- data/transformation/definitions.py
+++ data/transformation/definitions.py
@@ -5,29 +5,33 @@ import dagster as dg
 
 from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
 
+log = dg.get_dagster_logger()
+
 transformation_project = DbtProject(
     project_dir=Path(__file__).joinpath("..", "dbt_transform").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "dbt_project").resolve(),
 )
 
+log.info(f"project_dur: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
+log.info(f"packaged_dir: {Path(__file__).joinpath('..', 'dbt_transform').resolve()}")
+
 # If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at runtime.
 # Otherwise, we expect a manifest to be present in the project's target directory.
 
-# if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
-#     # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
-#     dbt_parse_invocation = (
-#         DbtCliResource(project_dir=transformation_project).cli(["parse"]).wait()
-#     )
-#     dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
-# else:
-#     dbt_manifest_path = transformation_project.project_dir.joinpath(
-#         "target", "manifest.json"
-#     )
-#
-
-dbt_manifest_path = transformation_project.project_dir.joinpath(
-    "target", "manifest.json"
-)
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    log.info("PARSE ENV VAR SET")
+    # Run dbt parse to generate the manifest. Adjust these calls based on your DbtCliResource's behavior.
+    dbt_parse_invocation = (
+        DbtCliResource(project_dir=transformation_project).cli(["parse"]).wait()
+    )
+    log.info(dbt_parse_invocation)
+    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+    log.info(dbt_manifest_path)
+else:
+    dbt_manifest_path = transformation_project.project_dir.joinpath(
+        "target", "manifest.json"
+    )
+    log.info(dbt_manifest_path)
 
 
 @dbt_assets(manifest=dbt_manifest_path)
