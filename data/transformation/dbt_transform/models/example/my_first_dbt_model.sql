01dc1c626e5eaf0429d3c8bea226ad731bb4a3bd
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-18 15:15:47 -0300

    test mode

--- data/transformation/dbt_transform/models/example/my_first_dbt_model.sql
+++ data/transformation/dbt_transform/models/example/my_first_dbt_model.sql
@@ -1,25 +1,16 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-
+WITH awards AS (
+  SELECT
+    tm."procurementEntity",
+    (award->>'awardAmount')::text::money AS award_amount
+  FROM {{ ref('tender_metadata') }} tm
+  CROSS JOIN LATERAL jsonb_array_elements(tm."tenderAwardData") AS award
+  WHERE award->>'awardAmount' ~ '^\d+(\.\d+)?$'
 )
 
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+SELECT
+  "procurementEntity",
+  SUM(award_amount) AS total_tender_value
+FROM awards
+GROUP BY "procurementEntity";
