04316b24cacb22f66f3d039437af1e72a4b59d0d
ldhwaddell/index-can <ldhwaddell@gmail.com>
2025-04-18 18:32:02 +0000

    ensure dbt checks quote cols, move to prod env in profile

--- data/transformation/dbt_transform/models/example/my_first_dbt_model.sql
+++ data/transformation/dbt_transform/models/example/my_first_dbt_model.sql
@@ -4,7 +4,7 @@ WITH awards AS (
   SELECT
     tm."procurementEntity",
     (award->>'awardAmount')::text::money AS award_amount
-  FROM {{ ref('tender_metadata') }} tm
+  FROM tender_metadata tm
   CROSS JOIN LATERAL jsonb_array_elements(tm."tenderAwardData") AS award
   WHERE award->>'awardAmount' ~ '^\d+(\.\d+)?$'
 )
@@ -13,4 +13,4 @@ SELECT
   "procurementEntity",
   SUM(award_amount) AS total_tender_value
 FROM awards
-GROUP BY "procurementEntity";
+GROUP BY "procurementEntity"
