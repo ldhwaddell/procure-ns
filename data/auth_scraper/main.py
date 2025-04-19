fbab10209c2bc9d4d8049ed64dcf4c4b77ba3ea7
Lucas Waddell <ldhwaddell@gmail.com>
2025-04-18 21:10:47 -0300

    move image, change to js

--- data/auth_scraper/main.py
+++
@@ -1,6 +0,0 @@
-from fake_useragent import UserAgent
-from playwright.sync_api import sync_playwright
-
-ua = UserAgent(platforms="desktop").random
-
-print(f"$$$$$$$$$$$$$$$$$$$$${ua}$$$$$$$$$$$$$$$$$$$$$$")
